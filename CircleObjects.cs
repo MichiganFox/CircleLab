d52b311594eaa548e5dc48683b0ac5df8b2f670c
Earle Fox <earle.fox312@gmail.com>
2023-01-24 09:04:12 -0500

    first commit

---
+++ CircleObjects.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+    public class Circle
+    {
+
+        public double radius { get; set; }
+
+        public Circle(double _radius) { radius = _radius; }
+
+        //methods 
+
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * radius;
+
+        }
+        public string CalculateFormattedCircumference()
+        {
+            string formattedCicumfrence = Math.Round(CalculateCircumference(), 2).ToString();
+            return formattedCicumfrence;
+
+        }
+
+        public double CalculateArea()
+        {
+            double area = radius * radius * Math.PI;
+            return area;
+
+        }
+        public string CalculateFormattedArea()
+        {
+            string formattedArea = Math.Round(CalculateArea(), 2).ToString();  
+            Math.Round()
+            return formattedArea;
+        }
+        private string FormatNumber(double x)
+        {
+            string formattedArea = Math.Round (x, 2).ToString();//the 2 is for the decimal places 
+            return formattedArea;
+        }
+
+        //public double CalculateArea()
+        //        {
+
+        //        }
+
+    }
+}
+
+//    public class Employee
+//    {
+//        //Properties 
+//        public string Name { get; set; }
+//        public decimal Salary { get; set; }
+//        public string Department { get; set; }
+
+//        //now that properties are set up -- need to set up constructor--see below 
+
+//        public Employee(string _name, decimal _salary, string _department)//salary will need an "M" because it is a decimal 
+//        {
+//            Name = _name;
+//            Salary = _salary;
+//            Department = _department;
+
+//        }
+//        public Employee() //This is both overloaded and default (in case user doesn't provide values
+//        {
+//            Name = "Justin";
+//            Salary = 5;
+//            Department = "Head of the Back Alley";
+//        }
+//        // methods 
+//        public string GetDetails()
+//        {
+//            return $"{Name}  Department {Department} Salary ${Salary}";
+//        }
+
+//    }
+//}
+
+
+////methods
+////public decimal GetInterestRate()
+////{
+////    return InterestRate;
+////}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using CircleObjects;
+using static System.Net.Mime.MediaTypeNames;
+//The application prompts the user to enter a radius; the user may enter a decimal number.
+
+
+int x = 1;
+bool goProgram = true;
+while (goProgram)
+{
+    Console.WriteLine("Please enter a radius.  You may use a decimal number.");
+    double i = int.Parse(Console.ReadLine());
+
+    //The application displays an error if the user enters invalid data and asks the user again for a radius
+    bool numverify = true;
+    while (numverify)
+    {
+        if (i < 0)
+        {
+            //Console.WriteLine($"You have entered {radi} as a radius for the cirle.");
+            //numverify = false;
+
+            Console.WriteLine("Please enter a number greater than 0");
+            return;
+        }
+        else
+        {
+            //Console.WriteLine("Please enter a number ");
+            Console.WriteLine($"You have entered {i} as a radius for the cirle.");
+            numverify = false;
+
+        }
+        //  When the user enters valid data, the application calculates the area and circumference of the circle and rounds to the nearest 2 decimal places and prints out both.
+        Circle myCircle = new Circle(i);
+        Console.WriteLine();
+        Console.WriteLine($"With a radius of {i}, the circumfrence is:  ");
+        Console.WriteLine(myCircle.CalculateCircumference());
+        Console.WriteLine();
+        Console.WriteLine("The formatted circumference results in:");
+        Console.WriteLine(myCircle.CalculateFormattedCircumference());
+        Console.WriteLine();
+        Console.WriteLine($"With a radius of {i}, the area of the circle is:  ");
+        Console.WriteLine(myCircle.CalculateArea());
+        Console.WriteLine();
+        Console.WriteLine("The formatted area results in:");
+        Console.WriteLine(myCircle.CalculateFormattedArea());
+        Console.WriteLine();
+
+
+
+        while (true)
+
+        {
+            Console.WriteLine("Do you want to go again? y/n");
+            string answr = Console.ReadLine().ToLower().Trim();
+
+            if (answr == "y")
+            {
+                goProgram = true;
+                x = x + 1;
+                break;
+
+
+            }
+            else if (answr == "n")
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Thank you for playing {x} times!");                
+                goProgram = false;
+                break;
+
+            }
+            else
+            {
+                Console.WriteLine("That was not a valid answer.  Please enter y/n");
+            }
+
+
+
+        }
+
+
+
+
+
+
+        //The application asks the user whether they want to do another.
+    }
+
+}
+
